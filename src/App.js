a7538c554ae2994bbf85f40bb4d7ccaccae7b85f
Nagaiyan <a151623@bestbuy.com>
2019-09-24 16:06:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import {CardList} from './components/card-list/card-list-component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters:[],
+      searchField:''
+    };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users =>this.setState({monsters:users}))
+    .catch(err => console.log('There is an error'))
+  }
+  render(){
+    return (
+      <div className="App">
+        <input type="text" placeholder='Monsters' onChange={e => this.setState({searchField:e.target.value})}/>
+        <CardList monsters={this.state.monsters}/>
+      </div>
+    );
+
+  }
 }
+
 
 export default App;
