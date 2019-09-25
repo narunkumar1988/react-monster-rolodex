e4351c770e3dafef1a983d70c7b8689bd4320bfb
Arun Kumar Nagaiyan <arunkumarnagaiyan@Aruns-MacBook-Air.local>
2019-09-25 22:33:16 +0530

    Updates

---
+++ precache-manifest.b0b6d5b39eb89a0cacd526bbbe6cec7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929ed5875cc696ed5442f23d772eda26",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "af52d4efbd19e4dbfeab",
+    "url": "/react-monster-rolodex/static/css/main.d9394ab8.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/react-monster-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "af52d4efbd19e4dbfeab",
+    "url": "/react-monster-rolodex/static/js/main.c05e4ccb.chunk.js"
+  },
+  {
+    "revision": "92625c3f2f940f29d485",
+    "url": "/react-monster-rolodex/static/js/runtime-main.95c6763b.js"
+  }
+]);
